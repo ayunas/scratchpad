4f05c21a1ec3c635f965c541ecbdd330b553cd26
Amir Yunas <ayunas@gmail.com>
2020-04-03 13:46:10 -0400

    created a singleton implementation using a universe analogy

--- design_patterns/singleton.py
+++ design_patterns/singleton.py
@@ -1,24 +1,56 @@
+class Universe:
+    __solo_instance = None
 
-class Singleton:
-    __amir = 'cool'
-
+    def __init__(self):
+        if Universe.__solo_instance:
+            raise Exception("There is Only 1 Universe in the whole wide Universe")
+        Universe.__solo_instance = self
+    
     @classmethod
-    def access_cls(cls):
-        print(cls.__amir)
-    
+    def get_universe(cls):
+        if cls.__solo_instance is None:
+            Universe()
+        return cls.__solo_instance
+
     @staticmethod
-    def access_static():
-        print(Singleton.__amir)
+    def get_uni():
+        if Universe.__solo_instance == None:
+            Universe()
+        return Universe.__solo_instance
+
+u = Universe()
+print(u)
+
+x = u.get_uni()
+print(x)
+
+
+
+
+
+
+
+
+# class Singleton:
+#     __amir = 'cool'
+
+#     @classmethod
+#     def access_cls(cls):
+#         print(cls.__amir)
     
-    def access(self):
-        print(self.__amir)
+#     @staticmethod
+#     def access_static():
+#         print(Singleton.__amir)
+    
+#     def access(self):
+#         print(self.__amir)
 
 
-Singleton.access_cls()
-s = Singleton()
-s.access()
-Singleton.access_static()
-print(Singleton._Singleton__amir)
+# Singleton.access_cls()
+# s = Singleton()
+# s.access()
+# Singleton.access_static()
+# print(Singleton._Singleton__amir)
 
 
 
